5e6a12979454eabc35e75f14318df14ba214cd64
rifqi <58549988+rifqi1808@users.noreply.github.com>
2019-12-06 21:08:02 +0700

    Update FINAL PROJECT.sh

--- Legacy Codes/FINAL PROJECT.sh
+++ Legacy Codes/FINAL PROJECT.sh
@@ -1,224 +1,177 @@
-clear
-i=0
-declare -a nama
-declare -a kode
-declare -a lahir
-declare -a mati
-declare -a agama
+n=0;
+exit=1;
 
-cetak()
-{
-	echo -e "Data yang telah dimasukkan : \n"			
-		for (( q=0; q<i;q++  ))
-		do
-			echo -e "Data ke $[q+1]\nNama Mayat            : ${nama[q]}\nNomor Kematian        : ${kode[q]}\nTempat, tanggal lahir : ${lahir[q]}\nTempat, tanggal wafat : ${mati[q]}\nAgama                 : ${agama[q]}\n"
-		done
-}
+	echo "Masukkan nama anda."
+	read nama
 
-while :;
-do
-	echo -e "Sistem Informasi Data Mayat pada Tempat Pemakaman Umum\n\n1. Masukkan data mayat\n2. Lihat data mayat\n3. Cari data mayat\n4. Perbarui data mayat\n5. Hapus data mayat\n6. Keluar program\n"
- 	echo -n "Masukkan pilihan : "
- 	read pilih
+	while [[ $exit=1 ]]; 
+	do
+	#statements
+	invoice = "BAJUI"1;
+	let invoice=$invoice+3
 
- 	if (("$pilih" == 1));   # INPUT DATA
- 	then 
-  		clear
- 	 	echo -n "Nama Mayat            : "
- 	 	read nama[$i]
- 	 	echo -n "Nomor Kematian        : "
-  		read kode[$i]
-	  	echo -n "Tempat, tanggal lahir : "
-  		read lahir[$i]
-		echo -n "Tempat, tanggal wafat : "
-  		read mati[$i]
-	  	echo -n "Agama                 : "
-  		read agama[$i]
- 	 	i=`expr $i + 1`
-		
-		if (("$i" >= 2))
-		then
-			for (( g=0; g<i-1; g++ ))
-			do
-	   			if (( "${kode[$i-1]}" == "${kode[g]}" ))
-				then
-		     			echo -e "\nNomor kematian ini sudah terdaftar!\nMasukan nomor yang lain!"
-					read
-					unset nama[$i-1]
-					unset kode[$i-1]
-					unset lahir[$i-1]
-					unset mati[$i-1]
-					unset agama[$i-1]
-					i=`expr $i - 1`
-					break
-	   			fi
-			done
+	increment = 0;
+	let increment=$increment+1
+
+	clear
+	echo "Hi $nama. Selamat datang di Toko Bangunan SukaBapak."
+	echo "1. Create User."
+	echo "2. Login User."
+	read login
+	if [[ $login -eq 1 ]];
+	then
+		echo "Buat  Username !"
+		echo -n "Username = "
+		read username;
+		echo "Username selesai."
+		echo "=========================="
+		echo "Buat Password !"
+		echo -n "Password = "
+		read password;
+		echo "Alhamdulillah, User kamu udah jadi."
+		echo "=========================="
+		let n=$n+1;
+	elif [[ $login -eq 2 ]]; then 
+		echo "Login !"
+		echo -n "Username = "
+		read usr;
+		echo "=========================="
+		echo "Password !"
+		echo -n "Password = "
+		read pwd;
+		echo "=========================="
+		if [[ $usr == $username && $pwd == $password ]];
+			then
+			echo "Kamu berhasil login."
+			break;
+		else
+			echo "Kamu Salah memasukkan password."
+			echo "Tolong ulangi lagi."
+			exit=1;
 		fi
- 	 	clear
- 
- 	elif (("$pilih" == 2));   # TAMPILKAN DATA
- 	then
-  		if (( i == 0 ))
-  		then
-   			clear
-   			echo "Tidak ada data yang dapat ditampilkan"
-   			read
-   			clear
-  		else
-   			clear
-   			cetak
-   			read
-   			clear
-  		fi
+		#statements
+	fi
+	done
 
- 	elif (("$pilih" == 3));   # CARI DATA
- 	then
-  		if (( i == 0))
-  		then
-   			clear
-   			echo "Tidak ada data yang dapat dicari"
-   			read
-   			clear
-  		else  
-   			clear
-      			echo -n "Masukkan nomor kematian yang ingin dicari : "
-      			read cari
-   
-      			k=0
-      			while (($cari != ${kode[$k]}))
-      			do
-    				k=`expr $k + 1`
-      			done
-      
-   			if (($cari == ${kode[$k]}));
-   			then
-    				echo -e "Nama Mayat            : ${nama[k]}\nNomor Kematian        : ${kode[k]}\nTempat, tanggal lahir : ${lahir[k]}\nTempat, tanggal wafat : ${mati[k]}\nAgama                 : ${agama[k]}\n"   
+exit=1;
+while [[ $exit = 1 ]];
+do
+echo ""
+echo "Menu"
+   echo "1. Pembelian";
+   echo "2. Pembayaran ";
+   echo "3. Cetak Struk ";
+   echo "4. KELUAR       ";
+read -p "Pilih nomer berapa nih? " pilihan
 
-   			else
-    				clear    
-    				echo -e "Data tidak ditemukan"
-   			fi
-   			read
-      			clear
-  		fi
-   
- 	elif (("$pilih" != 4 && "$pilih" != 2 && "$pilih" != 1 && "$pilih" != 3 && "$pilih" != 5 && "$pilih" != 6));   # APABILA MEMBERIKAN MASUKAN SELAIN 1-6
- 	then
-  		echo "Pilihan tidak valid"
-  		read
-  		clear
+if [ $pilihan -eq 1 ];
+then
+clear;
+       echo "=====================================";
+       echo "        MENU HARGA BARANG BANGUNAN ";
+       echo "=====================================";
+       echo "1. Semen       RP 32.000";
+       echo "2. Seng        RP 50.000";
+       echo "3. Kayu        RP 70.000";
+       echo "=====================================";
+       echo -n "Masukan Nama anda                 :";
+       read nama 
+       echo -n "Masukan Alamat anda               :";
+       read alamat
+       echo -n "E-mail        : "
+       read email
+       echo -n "No HP         : "
+       read hp
+       echo -n "Masukan jenis barang anda (1-3)    :";
+       read jenis
+       echo -n "Masukan jumlah barang yang di beli : "
+       read jum
+       echo "=====================================";
+       if [ $jenis -eq 1 ];
+       then
+	       kelas="Semen";
+	       tiket=32000;
+	       let total=jum*tiket;
+       elif [ $jenis -eq 2 ];
+       then
+	       kelas="Seng";
+	       tiket=50000;
+	       let total=jum*tiket; 
+       elif [ $jenis -eq 3 ];
+       then
+	       kelas="Kayu";
+	       tiket=70000;
+	       let total=jum*tiket;
+       else
+	       echo "Sorry, tidak tersedia"
+	       exit=0;
+       fi
+elif [ $pilihan -eq 2 ];
+then
+       echo "Pilihan Pembayaran."
+       echo "1. Online.";
+       echo "2. Offline.";
+       read -p "Masukkin Pilihan : " pembayaran
+       echo ""
+       if [ $pembayaran -eq 1 ];
+       then
+	       echo "1. Kartu Kredit."
+	       echo "2. Transfer Bank"
+	       read -p "Masukkin Pilihan : " bayar
+	       if [ $bayar -eq 1 ];
+	       then
+		       echo -n "Masukkan nomor kartu kredit anda : "
+		       read cc
+		       if [ $cc -eq 18081999 ];
+		       then
+			       echo "No Invoice : "$invoice
+			       echo "Pembayaran Berhasil."
+	       else
+	       echo "Inputan Salah."
+	   fi
+	       fi
+       elif [[ $bayar -eq 2 ]];
+       then
+	       echo "Masukkan nomor Kartu atm anda : "
+	       read atm
+	       echo "Masukkan otp yang telah dikirimkan ke nomor telepon anda."
+	       read otp
+	       if [ $otp -eq 18081999 ];
+	       then
+		       echo "No Invoice : "$invoice
+		       echo "Pembayaran anda telah terverifikasi."
+		       echo "Pembayaran Berhasil."
+	       else
+		       echo "Inputan salah !!"
+		       echo "Masukkan nomor atm yang benar"
+	       fi
+       echo ""
+       elif [[ $pembayaran -eq 2 ]]; 
+       then
+       echo "Pembayaran di kasir."
+       else
+       echo "Inputan anda salah."
+       fi
+       echo ""
 
-	elif (( "$pilih" == 5 ));   # HAPUS DATA
-	then
-		if (( i == 0))
-  		then
-   			clear
-   			echo "Tidak ada data yang dapat dihapus"
-   			read
-   			clear
-  		else  
-   			clear
-   			cetak
-			echo -n "Masukkan nomor kematian : "
-      			read cari
-   
-      			k=0
-      			while (($cari != ${kode[$k]}))
-      			do
-    				k=`expr $k + 1`
-      			done
-      
-   			if (($cari == ${kode[$k]}));
-   			then			
-    				unset nama[k]
-				unset kode[k]
-				unset lahir[k]
-				unset mati[k]
-				unset agama[k]
-
-				if [[ -z ${kode[k]} ]]
-				then
-					if (($i == 1))
-					then
-						i=0
-	
-					elif (($i == 2))
-					then
-						for ((f=$k; f<i; f++))
-						do
-							nama[f]=${nama[$f+1]}
-							kode[f]=${kode[$f+1]}
-							lahir[f]=${lahir[$f+1]}
-							mati[f]=${mati[$f+1]}
-							agama[f]=${agama[$f+1]}
-						done
-						i=`expr $i - 1`
-
-					elif (($i > 2))
-					then
-						if [[ -n ${kode[$k-1]} ]]
-						then
-							for (( d=$k; d<i; d++ ))
-							do
-								nama[d]=${nama[$d+1]}
-								kode[d]=${kode[$d+1]}
-								lahir[d]=${lahir[$d+1]}
-								mati[d]=${mati[$d+1]}
-								agama[d]=${agama[$d+1]}
-							done
-						fi
-						i=`expr $i - 1`
-					fi									
-				fi
-   			else
-    				clear    
-    				echo -e "Data tidak ditemukan"
-   			fi
-   			read
-      			clear
-  		fi
-
-	elif (("$pilih" == 4));   # MEMPERBAHARUI DATA
-	then
-		if (( i == 0))
-  		then
-   			clear
-   			echo "Tidak ada data yang dapat diperbarui"
-   			read
-   			clear
-  		else  
-   			clear
-			cetak
-      			echo -e -n "\nMasukkan nomor kematian yang ingin diperbarui : "
-      			read baru
-   
-      			l=0
-      			while (($baru != ${kode[$l]}))
-      			do
-    				l=`expr $l + 1`
-      			done
-      
-   			if (($baru == ${kode[$l]}));
-   			then
-				echo -n "Nama Mayat            : "
-		 	 	read nama[$l]
-			  	echo -n "Tempat, tanggal lahir : "
-		  		read lahir[$l]
-				echo -n "Tempat, tanggal wafat : "
-		  		read mati[$l]
-			  	echo -n "Agama                 : "
-		  		read agama[$l]
-
- 	 			clear
-   			else
-    				clear    
-    				echo -e "Data tidak ditemukan"
-				read
-   			fi
-      			clear
-  		fi
- 
- 	else    # KELUAR PROGAM
-  		exit
- 	fi
+elif [ $pilihan -eq 3 ];
+then
+       clear;
+       echo "DATA PEMBELI ";
+       echo "++++++++++++++++++++++++++++++++++++";
+       echo "No invoice  : $invoice"
+       echo "NAMA        : $nama";
+       echo "ALAMAT      : $alamat";
+       echo "KELAS       : $kelas";
+       echo "JUMLAH      : $jum";
+       echo "Nomor Hp    : $hp"
+       echo "E-mail      : $email"
+       echo "++++++++++++++++++++++++++++++++++++";
+       echo "TOTAL BAYAR : $total";
+       echo "++++++++++++++++++++++++++++++++++++";
+       echo
+else
+	exit=0;
+	echo "Arigatou Gozaimasu."
+fi
 done
